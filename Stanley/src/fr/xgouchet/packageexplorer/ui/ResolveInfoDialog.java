7cb1705643b54f29a031a3f22f970caf34e51050
Xavier GOUCHET <android@xgouchet.fr>
2013-03-21 16:38:40 +0100

    Multiple Home Activities

---
+++ Stanley/src/fr/xgouchet/packageexplorer/ui/ResolveInfoDialog.java
@@ -0,0 +1,54 @@
+package fr.xgouchet.packageexplorer.ui;
+
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListAdapter;
+import fr.xgouchet.packageexplorer.R;
+import fr.xgouchet.packageexplorer.common.Constants;
+import fr.xgouchet.packageexplorer.common.PackageUtils;
+import fr.xgouchet.packageexplorer.ui.adapter.ResolveInfoAdapter;
+
+public class ResolveInfoDialog extends DialogFragment {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.layout_resolve_info, container,
+				false);
+
+		getDialog().setTitle(R.string.action_open);
+
+		Bundle args = getArguments();
+		mInfoList = (ResolveInfo[]) args
+				.getParcelableArray(Constants.EXTRA_RESOLVE_INFO);
+
+		mGridView = (AdapterView<ListAdapter>) view
+				.findViewById(android.R.id.list);
+		mGridView.setAdapter(new ResolveInfoAdapter(getActivity(), mInfoList));
+		mGridView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
+				dismiss();
+				startActivity(PackageUtils
+						.getResolvedIntent(mInfoList[position]));
+
+			}
+		});
+
+		return view;
+	}
+
+	ResolveInfo[] mInfoList;
+	AdapterView<ListAdapter> mGridView;
+
+}
---
+++ Stanley/src/fr/xgouchet/packageexplorer/ui/adapter/ResolveInfoAdapter.java
@@ -0,0 +1,62 @@
+package fr.xgouchet.packageexplorer.ui.adapter;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import fr.xgouchet.packageexplorer.R;
+
+public class ResolveInfoAdapter extends ArrayAdapter<ResolveInfo> {
+
+	public ResolveInfoAdapter(Context context, ResolveInfo[] objects) {
+		super(context, R.layout.item_resolve_info, android.R.id.title, objects);
+		mLayoutInflater = LayoutInflater.from(getContext());
+		mPackageManager = context.getPackageManager();
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View view = convertView;
+		if (view == null) {
+			view = mLayoutInflater.inflate(R.layout.item_resolve_info, parent,
+					false);
+		}
+
+		ResolveInfo info = getItem(position);
+		ComponentName component = new ComponentName(
+				info.activityInfo.packageName, info.activityInfo.name);
+
+		TextView textView = (TextView) view.findViewById(android.R.id.title);
+		Drawable icon = null;
+		String title = null;
+
+		try {
+			icon = mPackageManager.getActivityIcon(component);
+		} catch (NameNotFoundException e) {
+			icon = getContext().getResources().getDrawable(
+					R.drawable.ic_no_launcher);
+		}
+
+		title = info.activityInfo.name;
+		// int lastDot = title.lastIndexOf('.');
+		// title = title.substring(lastDot);
+		if (title.startsWith(info.activityInfo.packageName)) {
+			title = title.substring(info.activityInfo.packageName.length());
+		}
+
+		textView.setCompoundDrawablesWithIntrinsicBounds(icon, null, null, null);
+		textView.setText(title);
+
+		return view;
+	}
+
+	private LayoutInflater mLayoutInflater;
+	private PackageManager mPackageManager;
+}

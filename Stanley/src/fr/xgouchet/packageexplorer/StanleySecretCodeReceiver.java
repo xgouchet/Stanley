bcc92f3c8f781fc442a1982829dd50880707d67b
Xavier Gouchet <xg@deezer.com>
2014-02-05 14:20:30 +0100

    Improved secret code system

--- Stanley/src/fr/xgouchet/packageexplorer/StanleySecretCodeReceiver.java
+++ Stanley/src/fr/xgouchet/packageexplorer/StanleySecretCodeReceiver.java
@@ -3,19 +3,35 @@ package fr.xgouchet.packageexplorer;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 
 public class StanleySecretCodeReceiver extends BroadcastReceiver {
 
+	private static final String INTENT_SECRET_CODE = "android.provider.Telephony.SECRET_CODE";
+	private static final String NUMBER_SECRET = "20041984";
+
 	/**
-	 * 
+	 * 5484647866
 	 */
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		if (intent.getAction().equals("android.provider.Telephony.SECRET_CODE")) {
-			Intent secret = new Intent(context, StanleySecretPreferences.class);
-			secret.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			context.startActivity(secret);
-		}
-	}
 
+		// Check intent action
+		if (!INTENT_SECRET_CODE.equals(intent.getAction())) {
+			return;
+		}
+
+		// Check code
+		Uri data = intent.getData();
+		String code = data.getHost();
+		if (!NUMBER_SECRET.equals(code)) {
+			return;
+		}
+
+		// Actually do it
+		Intent secret = new Intent(context, StanleySecretPreferences.class);
+		secret.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		context.startActivity(secret);
+
+	}
 }

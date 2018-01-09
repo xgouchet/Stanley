88a1dad4a8dcdf634a487899704b2c08a690bf03
Xavier Gouchet <xavier.gouchet@gmail.com>
2018-01-09 09:12:01 +0100

    Fix sort

--- app/src/main/java/fr/xgouchet/packageexplorer/applist/sort/AppInstallTimeComparator.kt
+++ app/src/main/java/fr/xgouchet/packageexplorer/applist/sort/AppInstallTimeComparator.kt
@@ -1,14 +1,24 @@
 package fr.xgouchet.packageexplorer.applist.sort
 
 import fr.xgouchet.packageexplorer.applist.AppViewModel
+import timber.log.Timber
+import kotlin.math.sign
 
 /**
  * @author Xavier F. Gouchet
  */
 object AppInstallTimeComparator : Comparator<AppViewModel> {
     override fun compare(lhs: AppViewModel?, rhs: AppViewModel?): Int {
+        val result: Int
         val lht = lhs?.installTime ?: 0
         val rht = rhs?.installTime ?: 0
-        return (rht - lht).toInt()
+        if (lht == rht) {
+            val lhn = lhs?.packageName?.toLowerCase() ?: ""
+            val rhn = rhs?.packageName?.toLowerCase() ?: ""
+            result = lhn.compareTo(rhn)
+        } else {
+            result = (rht - lht).sign
+        }
+        return result
     }
 }
--- app/src/main/java/fr/xgouchet/packageexplorer/applist/sort/AppUpdateTimeComparator.kt
+++ app/src/main/java/fr/xgouchet/packageexplorer/applist/sort/AppUpdateTimeComparator.kt
@@ -1,14 +1,23 @@
 package fr.xgouchet.packageexplorer.applist.sort
 
 import fr.xgouchet.packageexplorer.applist.AppViewModel
+import kotlin.math.sign
 
 /**
  * @author Xavier F. Gouchet
  */
 object AppUpdateTimeComparator : Comparator<AppViewModel> {
     override fun compare(lhs: AppViewModel?, rhs: AppViewModel?): Int {
+        val result: Int
         val lht = lhs?.updateTime ?: 0
         val rht = rhs?.updateTime ?: 0
-        return (rht - lht).toInt()
+        if (lht == rht) {
+            val lhn = lhs?.packageName?.toLowerCase() ?: ""
+            val rhn = rhs?.packageName?.toLowerCase() ?: ""
+            result = lhn.compareTo(rhn)
+        } else {
+            result = (rht - lht).sign
+        }
+        return result
     }
 }

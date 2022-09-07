ebbc5d022650b275ffd7c4e2139eb58271aae86d
dreamsyntax <dreamsyntax@gmail.com>
2022-09-06 17:35:19 -0700

    Dependencies, SDK, Platform updates Gradle 6.1.1 -> 7.5.1 AGP 4.0.2 -> 7.2.2 Kotlin 1.4.10 -> Kotlin 1.7.10 BuildTools 30.0.1 -> 33.0.0 TargetSdk 30 -> 32
    Bump versionCode and versionName
    
    Upgrade all dependencies
    Move unused const dependency versions to own category
    Explicit stdlib-jdk8 to ensure kotlin 1.7.10 used
    
    Restore JCenter for legacy dependencies until replacements implemented

--- buildSrc/src/main/kotlin/fr/xgouchet/gradle/Dependencies.kt
+++ buildSrc/src/main/kotlin/fr/xgouchet/gradle/Dependencies.kt
@@ -5,33 +5,33 @@ object Dependencies {
 
     object Versions {
 
-        const val AndroidPlugin = "4.0.0"
-
-        const val AndroidXAnnotations = "1.1.0"
-        const val AndroidXAppCompat = "1.2.0"
+        // unused
+        const val AndroidXNav = "2.2.0"
         const val AndroidXArch = "2.0.0"
-        const val AndroidXCore = "1.3.2"
-        const val AndroidXMedia = "1.2.0"
-        const val AndroidXNav = "2.1.0-alpha02"
-        const val AndroidXRecyclerView = "1.1.0"
         const val AndroidXTest = "1.1.0"
         const val AndroidMaven = "2.1"
-        const val MultiDex = "2.0.1"
-        const val ConstraintLayout = "2.0.2"
-        const val MaterialComponents = "1.2.1"
-        const val Gson = "2.8.6"
-
         const val BuildTimeTracker = "0.11.0"
         const val DependencyVersion = "0.33.0"
-        const val Detekt = "1.14.1"
-        const val Kotlin = "1.4.10"
-        const val RxJava = "3.0.0"
+        const val Detekt = "1.21.0"
+        const val Leakcanary = "2.9.1"
+        const val Stetho = "1.5.0"
+        // end unused
+
+        const val AndroidPlugin = "7.2.2"
+        const val AndroidXAnnotations = "1.4.0"
+        const val AndroidXAppCompat = "1.5.0"
+        const val AndroidXCore = "1.8.0"
+        const val AndroidXMedia = "1.6.0"
+        const val AndroidXRecyclerView = "1.2.1"
+        const val MultiDex = "2.0.1"
+        const val ConstraintLayout = "2.1.4"
+        const val MaterialComponents = "1.6.1"
+        const val Gson = "2.9.1"
+        const val Kotlin = "1.7.10"
+        const val RxJava = "3.0.13"
         const val RxAndroid = "3.0.0"
         const val AboutPage = "1.3"
-
-        const val Leakcanary = "1.5.4"
         const val Timber = "4.7.1"
-        const val Stetho = "1.5.0"
         const val AXml = "v1.0.1"
     }
 
@@ -54,6 +54,7 @@ object Dependencies {
 
         @JvmField
         val Kotlin = arrayOf(
+                "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.Kotlin}",
                 "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.Kotlin}",
                 "org.jetbrains.kotlin:kotlin-reflect:${Versions.Kotlin}"
         )
